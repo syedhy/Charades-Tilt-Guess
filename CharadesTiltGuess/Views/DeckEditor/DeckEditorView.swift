d468a98c481385b41a3e3417b309293b9322cc60
Syed Hyder <syedhy@users.noreply.github.com>
2026-06-06 14:40:43 +0530

    fix(deck-editor): make keyboard scroll snappier

--- CharadesTiltGuess/Views/DeckEditor/DeckEditorView.swift
+++ CharadesTiltGuess/Views/DeckEditor/DeckEditorView.swift
@@ -194,7 +194,7 @@ struct DeckEditorView: View {
 
     private func scrollNameFieldIntoKeyboardView(using scrollProxy: ScrollViewProxy) {
         DispatchQueue.main.asyncAfter(deadline: .now() + Self.keyboardScrollDelay) {
-            withAnimation(.snappy(duration: 0.34)) {
+            withAnimation(.snappy(duration: 0.18)) {
                 scrollProxy.scrollTo(Self.deckNameFieldScrollID, anchor: .center)
             }
         }
@@ -203,7 +203,7 @@ struct DeckEditorView: View {
 
 private extension DeckEditorView {
     static let deckNameFieldScrollID = "deck-name-field-scroll-target"
-    static let keyboardScrollDelay: TimeInterval = 0.32
+    static let keyboardScrollDelay: TimeInterval = 0.08
 }
 
 #Preview {
